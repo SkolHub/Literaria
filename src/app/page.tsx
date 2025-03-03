5325a5de664b70ce8647808481eb51e9bc241ed3
karo <cazacuchristian@gmail.com>
2025-03-03 22:21:06 +0200

    Made site better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Landing from '@/app/_sections/landing';
 import Latest from '@/app/_sections/latest';
 import Recommended from '@/app/_sections/recommended';
 
 export default function () {
   return (
     <>
-      <Landing />
+      {/*<Landing />*/}
       <Recommended />
       <Latest />
     </>
